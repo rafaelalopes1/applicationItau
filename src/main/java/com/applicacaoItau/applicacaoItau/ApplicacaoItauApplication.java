392195786c7bd90cf8300201393681892e079499
rafaelalopes1 <rafaelamartins64@hotmail.com>
2023-10-02 14:11:17 -0300

    subindo arquivo de banco de dados

--- src/main/java/com/applicacaoItau/applicacaoItau/ApplicacaoItauApplication.java
+++ src/main/java/com/applicacaoItau/applicacaoItau/ApplicacaoItauApplication.java
@@ -1,13 +1,39 @@
 package com.applicacaoItau.applicacaoItau;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApplicacaoItauApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicacaoItauApplication.class, args);
-	}
+
+
+	
+//	Scanner ler = new Scanner(System.in);
+
+//    ArrayList<String> agenda = new ArrayList();
+//    agenda.add("Rafa;87803928");
+//    agenda.add("gui;56220105");
+//    System.out.print("entrou aqui");
+//    for (String lista : agenda) {
+//        System.out.print("entrou aqui for");
+//    	if (lista.equals("Rafa;87803928")) {
+//    		System.out.print("achou Rafa");
+//    	}
+//    }
+    
+
+
+}
+
+
+		
+		    
+	
+	
 
 }
--- src/main/java/com/applicacaoItau/applicacaoItau/model/RetornarLista.java
+++
@@ -1,6 +0,0 @@
-package com.applicacaoItau.applicacaoItau.model;
-
-public class RetornarLista {
-
-	private String Lista;
-}
