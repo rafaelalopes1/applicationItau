cc15d2e2b19d7c429c8a2fabfc280b237e490db1
rafaelalopes1 <rafaelamartins64@hotmail.com>
2023-10-01 21:02:32 -0300

    teste commit 4

---
+++ src/main/java/com/applicacaoItau/applicacaoItau/ApplicacaoItauApplication.java
@@ -0,0 +1,13 @@
+package com.applicacaoItau.applicacaoItau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicacaoItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicacaoItauApplication.class, args);
+	}
+
+}
