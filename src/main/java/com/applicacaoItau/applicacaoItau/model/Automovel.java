9e8fba9ca354bc722d32805e35af0e5d40707623
rafaelalopes1 <rafaelamartins64@hotmail.com>
2023-10-02 16:38:43 -0300

    get e post tabela automovel.

--- src/main/java/com/applicacaoItau/applicacaoItau/model/Automovel.java
+++ src/main/java/com/applicacaoItau/applicacaoItau/model/Automovel.java
@@ -15,7 +15,7 @@ import jakarta.persistence.Table;
 public class Automovel {
 	
 	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String marca;
